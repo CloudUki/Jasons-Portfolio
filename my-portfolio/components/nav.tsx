32699890ad4a8509daa1c5d7c047d3b2e06a5aef
Brandon Yang <brandon@Brandons-MacBook-Pro.local>
2025-11-23 00:34:21 -0500

    added resume, and added homepage text

--- my-portfolio/components/nav.tsx
+++ my-portfolio/components/nav.tsx
@@ -1,29 +1,29 @@
 export default function Navbar() {
     return (
-      <nav className="w-full bg-neutral-100 border-b border-neutral-300 py-4 fixed top-0 left-0">
+      <nav className="w-full bg-yellow-100 border-b border-yellow-100 py-4 fixed top-0 left-0">
         <ul className="flex justify-center gap-16 text-xl font-serif text-black">
           <li>
-            <a href="#home">
+            <a href="#home" className="hover:text-red-600 transition">
               Home
             </a>
           </li>
           <li>
-            <a href="#about">
+            <a href="#about" className="hover:text-red-600 transition">
               About
             </a>
           </li>
           <li>
-            <a href="#experiences">
+            <a href="#experiences" className="hover:text-red-600 transition">
               Experiences
             </a>
           </li>
           <li>
-            <a href="#projects">
+            <a href="#projects" className="hover:text-red-600 transition">
               Projects
             </a>
           </li>
           <li>
-            <a href="#contact">
+            <a href="#contact" className="hover:text-red-600 transition">
               Contact
             </a>
           </li>
